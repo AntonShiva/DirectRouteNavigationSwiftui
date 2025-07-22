16e6dc754cebcc5521e2647869d8d0030c28fc40
AntonShiva <ryashinanthon@yandex.ru>
2025-07-22 15:55:45 +0300

    Initial Commit

---
+++ NavigationTheBeast/NavigationTheBeastApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationTheBeastApp.swift
+//  NavigationTheBeast
+//
+//  Created by Anton Reasin on 22.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct NavigationTheBeastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
